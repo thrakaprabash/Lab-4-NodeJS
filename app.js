ce7462680b476e502dc250a0a058ea47fbe2678c
tharakaprabash <tplstlsml@gmail.com>
2026-02-16 12:04:52 +0530

    Initial Node setup with Write file

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
   console.log(data);
 });
 
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
+
+
 
